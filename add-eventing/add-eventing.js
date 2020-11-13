45e830e11b0294402d26be14f121082d2775ca5f
katfernandez22 <60719712+katfernandez22@users.noreply.github.com>
2020-11-12 22:43:30 -0500

    Add files via upload

--- add-eventing/add-eventing.js
+++ add-eventing/add-eventing.js
@@ -1,4 +1,21 @@
 const addEventing = function (obj) {
 }
+const addEventing = obj => {
+  let arr = [];
+  let newObj = {
+    on: (step, callback) => {
+      if (!arr[step]) {
+        arr[step] = [];
+      }
+      arr[step].push(callback);
+    },
+    trigger: (step, ...args) => {
+      if (arr[step]) {
+        arr[step].map((callback) => callback(...args));
+      }
+    }
+  };
+  return newObj;
+};
 
 module.exports = addEventing
